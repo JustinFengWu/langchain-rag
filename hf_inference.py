8befa51fd2432d21b385c85d2347beee8641f940
JustinFengWu <a1891628@adelaide.edu.au>
2024-10-05 21:13:40 +0930

    Hide API KEY

--- hf_inference.py
+++ hf_inference.py
@@ -1,8 +1,9 @@
 from huggingface_hub import InferenceClient
+import os
 
 client = InferenceClient(
     "cardiffnlp/twitter-roberta-base-sentiment-latest",
-    token="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",
+    token=os.environ["HF_KEY"],
 )
 
 client.text_classification("Today is a great day")
